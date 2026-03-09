b348dbe21717bcd5291f89550c0468f05e49d93e
ajswldp <kkhhjj1209@gmail.com>
2026-03-09 10:57:41 +0900

    feat: initial commit

---
+++ src/main/kotlin/dontstopjo/ootdrop/OotDropApplication.kt
@@ -0,0 +1,11 @@
+package dontstopjo.ootdrop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OotDropApplication
+
+fun main(args: Array<String>) {
+    runApplication<OotDropApplication>(*args)
+}
---
+++ src/test/kotlin/dontstopjo/ootdrop/OotDropApplicationTests.kt
@@ -0,0 +1,13 @@
+package dontstopjo.ootdrop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OotDropApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
